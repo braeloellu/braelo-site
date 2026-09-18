7a2c9e926475df5e8e15a4725dc67cb7ac889119
Criss <braelo.ellu@gmail.com>
2026-09-18 14:41:29 -0400

    Add Braelo official contact information - contato@braelo.com / 56 1234 3806

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,6 +8,10 @@ export default function Header() {
           <span className={styles.logoBg}>b</span>
           <span className={styles.logoText}>braelo</span>
         </a>
+        <div className={styles.contact}>
+          <span>contato@braelo.com</span>
+          <span>56 1234 3806</span>
+        </div>
         <nav className={styles.nav}>
           <ul>
             <li><a href="#como-funciona">Como funciona</a></li>
